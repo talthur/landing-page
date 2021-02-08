82e128f6fbc4af7a74c9abf5a06feef80da6fe2b
Arthur Gomes <talthur.ga@gmail.com>
2021-02-08 09:29:25 -0300

    tentando arrumar links

--- scripts/app.js
+++ scripts/app.js
@@ -43,7 +43,7 @@ bullets.addEventListener("click", (e) => {
     
 });
 
-function activeBullet(e) {git
+function activeBullet(e) {
     console.log(e);
     document.querySelector("#active").id = "";
     e.target.id = "active";
