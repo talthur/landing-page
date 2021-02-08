86279705649204b2a41670712efefb6a85ba6269
Arthur Gomes <talthur.ga@gmail.com>
2021-02-08 18:36:05 -0300

    implentação de arrows

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,27 @@ menu = document.querySelector(".menu");
 hamburguerClick = document.querySelector(".hamburger");
 bullets = document.querySelector(".nav__bullets");
 photo = document.querySelector("#photo");
+containerArrows = document.querySelector(".container__arrows");
+navBullets = document.querySelector(".nav__bullets")
 
-listPhotos = ["1.jpg", "2.jpg", "3.jpg"]
+listPhotos = [
+    {
+        photo:"1.jpg",
+        bullet:0
+    },
+
+    {
+
+        photo:"2.jpg",
+        bullet:1
+    },
+
+    {
+        photo:"3.jpg",
+        bullet:2
+    }
+    
+]
 
 hamburguerClick.addEventListener("click", () => {
     
@@ -46,5 +65,35 @@ bullets.addEventListener("click", (e) => {
 function activeBullet(e) {
     console.log(e);
     document.querySelector("#active").id = "";
-    e.target.id = "active";
+    navBullets.children[pos].id = "active";
+    
 }
+
+let pos = 0
+
+containerArrows.addEventListener("click", (e) => {
+
+    if (e.target.id == "arrowr"){
+        if (pos == 2){
+            pos = 0;
+            activeBullet(pos);
+        }
+        else{
+            pos += 1;
+            activeBullet(pos);
+        }
+    };
+
+    if (e.target.id == "arrowl"){
+        if (pos == 0){
+            pos = 2;
+            activeBullet(pos);
+        }
+        else{
+            pos -= 1;
+            activeBullet(pos);
+        }
+    };
+    photo.src = `images/${listPhotos[pos].photo}`;
+
+})
