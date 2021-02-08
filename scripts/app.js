f507b89e13eb24d90e13bd592cdc74793d990689
Arthur Gomes <talthur.ga@gmail.com>
2021-02-07 21:50:15 -0300

    criação de carrosel e links

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 menu = document.querySelector(".menu");
 hamburguerClick = document.querySelector(".hamburger");
-icone = 
+bullets = document.querySelector(".nav__bullets");
+photo = document.querySelector("#photo");
+
+listPhotos = ["1.jpg", "2.jpg", "3.jpg"]
 
 hamburguerClick.addEventListener("click", () => {
     
@@ -13,4 +16,35 @@ hamburguerClick.addEventListener("click", () => {
         menu.style.display = "block";
     }
 
-})
+});
+
+bullets.addEventListener("click", (e) => {
+
+    if (e.target.dataset.id == "0"){
+        e.preventDefault()
+        activeBullet(e)
+        photo.src = `images/${listPhotos[0]}`;
+        
+    }
+
+    if (e.target.dataset.id == "1"){
+        e.preventDefault()
+        activeBullet(e)
+        photo.src = `images/${listPhotos[1]}`;
+        
+    }
+
+    if (e.target.dataset.id == "2"){
+        e.preventDefault()
+        activeBullet(e)
+        photo.src = `images/${listPhotos[2]}`;
+        
+    }
+    
+});
+
+function activeBullet(e) {
+    console.log(e);
+    document.querySelector("#active").id = "";
+    e.target.id = "active";
+}
